daddf8b0bc6654058f4256d79771d4aa8480983d
DRACCO <anderson.duarte@drac.co>
2023-09-01 17:43:03 -0300

    Delete tarefa1.cs

--- tarefa1.cs
+++
@@ -1,2 +0,0 @@
-/*Considerando um array com uma sequencia de pares (pai, filho), crie o código necessário para resolver o array de
-forma que o resultado seja uma árvore que representa a estrutura hierárquica do array inicial.*/
